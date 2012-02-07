014d94e77fae1db0242d820481769e85cf8f3f6b
Gerald Nilles <geraldnilles@gmail.com>
2012-02-06 19:02:22 -0600

    Added Code

---
+++ code.c
@@ -0,0 +1,60 @@
+// Code
+
+
+// Global Variables
+unsigned char period = 0xff; // Period of cycle in seconds
+unsigned char duty = 100; // Duty cycle of Heating Element
+unsigned char counter = 0; // Keeps track of current position in cycle
+unsigned char set_temp = 140;
+unsigned char curr_temp = 0;
+
+int main(){
+
+	// Setup the Timer Register to fire once a second
+	// Set all of the GPIO Input/Outputs
+	//
+}
+
+
+// Timer Interupt Routine
+// This function will be called once a second (roughly)
+void Timer_Interrupt(){
+	counter++;
+	if (counter <= duty){
+		// Turn on Heater/pump
+	}else{
+		// Turn off Heater/Pump
+	}
+
+	if(counter >= period){ // End of Cycle Reached
+		counter = 0;
+		// Calculate the next cycle's duty cycle using PID
+		set_duty_cycle();
+	}
+	// Toggle Heatbeat LED
+
+	// Setup ADC to do a single read
+}
+
+// ADC Interrupt Routine
+// This function will be called once the ADC has finished reading the 
+// temperature of the water.
+void ADC_Interrupt(){
+	// Read ADC Register
+	// Convert to Degrees F
+	update_LCD();
+}
+
+void update_LCD(){
+	// Send the Current Temp and Set temp to the Display screen
+}
+
+unsigned int integral = 0
+unsigned char prev_error = 0
+void set_duty_cydle(){
+	unsigned char error = set_temp - curr_temp;
+	integral = integral + (int) error;
+	unsigned char de_dt = error - prev_error;
+
+	duty = kp*error + kd*de_dt + ki*integral;
+}
