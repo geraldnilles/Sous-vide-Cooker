f2c77860763ca81b2515f3d802f980ccc8e5c23c
Gerald Nilles <geraldnilles@gmail.com>
2013-12-25 14:30:18 -0600

    Fix Float division issue with duty cycle

--- code/simple_controller.py
+++ code/simple_controller.py
@@ -3,8 +3,8 @@ import hardware
 
 set_temp = 130
 
-on_time = 0
-off_time = 0
+on_time = 0.0
+off_time = 0.0
 
 while(1):
 	temp = hardware.read_temperature()
@@ -13,17 +13,17 @@ while(1):
 	if on_time == 0 and off_time == 0:
 		pass
 	else:
-		print "Duty Cycle: ",on_time / (off_time+on_time)
+		print "Duty Cycle: ",int((on_time / (off_time+on_time))*100),"%"
 
 
 	if temp < set_temp:
 		# Temp too low, enabling heater
 		hardware.enable_heater()
-		on_time += 1
+		on_time += 1.0
 	else:
 		# Temp too high, disabling heater
 		hardware.disable_heater()
-		off_time += 1
+		off_time += 1.0
 
 	# Wait 30 seconds
 	time.sleep(30)
